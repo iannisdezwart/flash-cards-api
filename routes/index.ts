16fab583785acf4dd4469f734e3da3807eca02dd
Iannis de Zwart <info@iannis.io>
2022-08-01 00:15:30 +0200

    🗣 Add Text To Speech Endpoint

--- routes/index.ts
+++ routes/index.ts
@@ -1,2 +1,3 @@
 import './auth/index.js'
 import './sets/index.js'
+import './tts/index.js'
---
+++ routes/tts/speak.ts
@@ -0,0 +1,88 @@
+import { readJSONBody } from '@iannisz/node-api-kit'
+import { ServerResponse } from 'http'
+import { AudioConfig, AudioOutputStream, SpeechConfig, SpeechSynthesizer, VoiceInfo } from 'microsoft-cognitiveservices-speech-sdk'
+import { api } from '../../api.js'
+
+const defaultSpeechConfig = SpeechConfig.fromSubscription(process.env.AZURE_SPEECH_KEY, process.env.AZURE_SPEECH_REGION)
+const defaultSynthesiser = new SpeechSynthesizer(defaultSpeechConfig)
+let allVoices: VoiceInfo[]
+
+const getVoiceName = async (locale: string, gender: 'male' | 'female') =>
+{
+	if (allVoices == null)
+	{
+		allVoices = (await defaultSynthesiser.getVoicesAsync()).voices
+	}
+
+	const voices = allVoices.filter(voice => voice.locale == locale)
+	const preferredGender = gender == 'female' ? 1 : 2
+	const preferredGenderVoice = voices.find(voice => voice.gender == preferredGender)
+
+	if (preferredGenderVoice != null)
+	{
+		return preferredGenderVoice.shortName
+	}
+
+	return voices[0]?.shortName
+}
+
+const speak = (voiceName: string, text: string, res: ServerResponse) =>
+{
+	const speechConfig = SpeechConfig.fromSubscription(process.env.AZURE_SPEECH_KEY, process.env.AZURE_SPEECH_REGION)
+	speechConfig.speechSynthesisVoiceName = voiceName
+
+	const synthesiser = new SpeechSynthesizer(speechConfig)
+
+	synthesiser.speakTextAsync(text, async result =>
+	{
+		res.end(Buffer.from(result.audioData))
+		synthesiser.close()
+	}, err =>
+	{
+		console.error(err)
+		synthesiser.close()
+	})
+}
+
+api.get('/tts/speak', async (req, res) =>
+{
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Content-Type', 'audio/wav')
+
+	const url = new URL(req.url, 'http://localhost')
+	const locale = url.searchParams.get('locale')
+	const text = url.searchParams.get('text')
+	const gender = url.searchParams.get('gender') || 'male'
+
+	if (locale == null || text == null)
+	{
+		res.statusCode = 400
+		res.end(JSON.stringify({
+			err: 'Please provide a "locale" and a "text" search parameter.'
+		}))
+	}
+
+	if (gender != 'male' && gender != 'female')
+	{
+		res.statusCode = 400
+		res.end(JSON.stringify({
+			err: 'Please provide a "gender" search parameter with the value of "male" or "female", or leave it empty.'
+		}))
+
+		return
+	}
+
+	const voiceName = await getVoiceName(locale, gender)
+
+	if (voiceName == null)
+	{
+		res.statusCode = 404
+		res.end({
+			err: 'Did\'t find a voice for the selected locale.'
+		})
+
+		return
+	}
+
+	speak(voiceName, text, res)
+})
